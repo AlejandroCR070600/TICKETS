6a0aef23c6d911d0ac4fc6d4e2e9d312b59ff2dd
NickAle98 <xxalinestarxx@gmail.com>
2025-07-16 15:16:14 -0600

    local para agregar el nuevo

---
+++ PHP/clases/ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+    class ticket{
+        public$fecha_Abierto;
+        public $hora_Abierto;
+        public $folio;
+        public $sucursal;
+        public $usuario;
+        public $telefono;
+        public $problema;
+        public $descripcion;
+        public $ipEquipo;
+    }
+
+?>
