e2ddb491182cd0f409b63222b073e075b902ef69
NickAle98 <xxalinestarxx@gmail.com>
2025-07-21 23:13:25 -0600

    se agrego forms_ADD y forms_ADD.js

---
+++ JS/forms_ADD.js
@@ -0,0 +1,27 @@
+let btnFormSucursal = document.getElementById("btnFormSucursal");
+let btnFormProblema = document.getElementById("btnFormProblema");
+let btnFormAdmin = document.getElementById("btnFormAdmin");
+let btnFormEquipo = document.getElementById("btnFormEquipo");
+let btnFormSupervisor = document.getElementById("btnFormSupervisor");
+let btnFormArea = document.getElementById("btnFormArea");
+let datos={};
+
+btnFormSucursal.addEventListener('click', function(event){
+
+});
+
+btnFormProblema.addEventListener('click', function(event){
+
+});
+btnFormAdmin.addEventListener('click', function(event){
+
+});
+btnFormEquipo.addEventListener('click', function(event){
+
+});
+btnFormSupervisor.addEventListener('click', function(event){
+
+});
+btnFormArea.addEventListener('click', function(event){
+
+});
